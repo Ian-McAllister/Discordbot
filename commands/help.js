96f2d111b9037657877a74622f94cb34ed862722
Ian-McAllister <77076197+Ian-McAllister@users.noreply.github.com>
2021-01-09 22:45:54 -0500

    Add files via upload
    Added what I have updated and worked on

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "help",
     description: "shows commands that are currently supported.",
     execute(message, args) {
-        message.channel.send('The commands that are currently working are:\nHelp\nTest\n');
+        message.channel.send('The commands that are currently working are:\nHelp\nTest\nscramble');
     },
 };
