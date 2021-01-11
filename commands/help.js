0b73802861e099b3ddb69bb1b4bb92e035498f80
Ian-McAllister <77076197+Ian-McAllister@users.noreply.github.com>
2021-01-11 15:58:23 -0500

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "help",
     description: "shows commands that are currently supported.",
     execute(message, args) {
-        message.channel.send('The commands that are currently working are:\nHelp\nTest\nscramble');
+        message.channel.send('The commands that are currently working are:\nHelp\nTest\nscramble\nrps\n\nTo use a command just type c! and the command name');
     },
 };
